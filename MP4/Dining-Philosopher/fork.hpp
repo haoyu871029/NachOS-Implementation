8478732a716d7cbb55f907b3618d5916885a49d5
Ricky Chuang <ss111062692@gapp.nthu.edu.tw>
2023-06-28 17:39:30 +0800

    upload

---
+++ MP4/Dining-Philosopher/fork.hpp
@@ -0,0 +1,19 @@
+#ifndef FORK_HPP
+#define FORK_HPP
+
+#include <pthread.h>
+#include <stdio.h>
+
+class Fork {
+public:
+    Fork();
+    void wait();
+    void signal(); 
+    ~Fork();
+private:
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+    int value;
+};
+
+#endif // FORK_HPP
